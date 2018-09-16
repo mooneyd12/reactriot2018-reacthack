0d7e6d2aa4019e6e9d0d5ea939856b4d64eea011
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-09-16 23:03:54 +0100

    Add fetch request outline

--- src/RoadStatus.js
+++ src/RoadStatus.js
@@ -5,6 +5,21 @@ class RoadStatus extends React.Component {
   constructor(props){
     super(props);
   }
+
+  componentDidMount(){
+    const url = "https://api-argon.tfl.gov.uk/Line/Mode/tube%2C%20dlr/Status?app_id=" + myKeys.appId + "&app_key=" + myKeys.appKey;
+    fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => {
+
+    })
+    .catch((err) =>{
+      console.log('Failed to fetch data');
+    })
+  }
+
   render(){
     return (
       <div className="status">
