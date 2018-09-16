90246ff546bc2a5516e6dc7e4d7f031aa6b0a5ae
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-09-16 22:50:58 +0100

    Create and add new component to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Status from './Status.js';
+import RoadStatus from './RoadStatus.js';
 
 class App extends Component {
 
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <h2 className="brand-name">Status<span>Report</span></h2>
         <Status />
+        <RoadStatus />
       </div>
     );
   }
---
+++ src/RoadStatus.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import myKeys from './config.js';
+
+class RoadStatus extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default RoadStatus;
