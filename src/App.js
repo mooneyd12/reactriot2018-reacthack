e46484fa95790aebe83c3b93995e583cfff4f497
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-09-16 03:06:42 +0100

    Add Status component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Status from './Status.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
   render() {
     return (
       <div>
-      
+        <h2 className="brand-name">TravelMapper</h2>
+        <Status />
       </div>
     );
   }
