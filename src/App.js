0519d9d68ffcfa00752ea7aa16678d4ebd79b0dc
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-09-16 19:59:25 +0100

    Update brand-name and styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import Status from './Status.js';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-  }
 
   render() {
     return (
       <div>
-        <h2 className="brand-name">TravelMapper</h2>
+        <h2 className="brand-name">Status<span>Report</span></h2>
         <Status />
       </div>
     );
