3fa673f84af32b1f707f9fc26718c1bb8b29941b
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-09-16 01:45:50 +0100

    Create a component called Status

---
+++ src/Status.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Status extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+
+  }
+}
+  render(){
+    return <div>
+      <h3 className="status-header">Status</h3>
+        <div>
+
+        </div>
+      </div>
+  }
+}
+
+export default Status;
