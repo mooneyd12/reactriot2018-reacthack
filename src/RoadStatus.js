3c2d051a321d2a93ee7e35ad1b78fd4e8aa8b2b8
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-09-16 23:25:56 +0100

    Fix fetch request error

--- src/RoadStatus.js
+++ src/RoadStatus.js
@@ -10,27 +10,26 @@ class RoadStatus extends React.Component {
   }
 
   componentDidMount(){
-    const url = "https://api-argon.tfl.gov.uk/Line/Mode/tube%2C%20dlr/Status?app_id=" + myKeys.appId + "&app_key=" + myKeys.appKey;
+    const url = "https://api.tfl.gov.uk/Road/a2/Disruption?closures=true&app_id="+ myKeys.apiId + "&app_key=" + myKeys.apiKey;
     fetch(url)
     .then((data) => {
       return data.json();
     })
     .then((res) => {
+      console.log(res);
       let accidentRow = res.map((results) => {
         return (
-          <div key={res.id} className="accidents-content">
-          <h5 className="location-name">{res.location}</h5>
-          <p className="update-desc">{res.currentUpdate}</p>
+          <div key={results.id} className="accidents-content">
+          <h5 className="location-name">{results.location}</h5>
+          <p className="update-desc">{results.currentUpdate}</p>
+          <p className="update-desc">{results.comments}</p>
           </div>
-        )
-      })
+        );
+      });
       this.setState({
         accidentRow: accidentRow
-      })
-    })
-    .catch((err) =>{
-      console.log('Failed to fetch data');
-    })
+      });
+    });
   }
 
   render(){
